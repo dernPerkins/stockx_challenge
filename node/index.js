48e0976096f45aa9aefc2c5e548389d57ea3cb06
dernPerkins <deanperkins93@gmail.com>
2019-10-18 23:25:49 -0400

    Added Logging and Use of Pool Connections.

--- node/index.js
+++ node/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
 const express = require('express');
-const { Client } = require('pg')
+const { Pool } = require('pg')
 
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
-const connectionString = 'postgresql://stockx:challenge@db:5432/stockx'
-const client = new Client({
-  connectionString: connectionString,
+const pool = new Pool()
+const errorLogType = 'Error';
+const infoLogType = 'Info';
+const debugLogType = 'Debug';
+
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
 })
+
 
 // Appf
 const app = express();
@@ -17,39 +25,44 @@ app.get('/', (req, res) => {
   res.send('Hello world\n');
 }).get('/testdbconnection', (req, res) => testDB(req, res));
 
-async function testDB(req, res) {
+// Log Helper Function that'll attempt to log but if the logging fails, the most we can do is output to console for monitoring.
+async function log(type, message) {
   try {
-    client.connect();
-    await client.query('SELECT $1::text as message', ['Hello world!'], (err, result) => {
-      try {
-        console.log(err ? err.stack : result.rows[0].message) // Hello World!
-        client.end(err => {
-          console.log('client has disconnected line 27');
-          if (err) {
-            console.log('error during disconnection'. err.stack);
-          }
-        });
-        res.send({ success: 'Hello World!'});
-      } catch (error) {
-        client.end(err => {
-          console.log('client has disconnected line 35');
-          if (err) {
-            console.log('error during disconnection'. err.stack);
-          }
-        });
-        res.send({ error: error.message,
-          errorStack: error.stack });
-      }
+    pool.connect((err, client, done) => {
+      if (err) throw err
+      client.query('insert into Logs (Type, Message) values ($1, $2)', [type, message], (error, result) => {
+        done();
+        if (error) {
+          console.log(error.stack);
+        }
+      })
     });
   } catch (error) {
-    client.end(err => {
-      console.log('client has disconnected line 45');
-      if (err) {
-        console.log('error during disconnection'. err.stack);
-      }
+    console.log(error.stack);
+  }
+}
+
+async function testDB(req, res) {
+  try {
+    pool.connect((err, client, done) => {
+      if (err) throw err
+      client.query('select * from Shoes', (error, result) => {
+        done();
+        if (error) {
+          console.log(`Error Message: ${error.message}; Error Stack: ${error.stack}`);
+          res.send({ error: "Failed Connection, check the logs." });
+          log(errorLogType, error.message);
+        } else {
+          console.log(`Successful Response: { success: ${JSON.stringify(result.rows[0])} }`);
+          res.send({ success: result.rows[0] });
+          log(infoLogType, `Successful Response: { success: ${JSON.stringify(result.rows[0])} }`);
+        }
+      })
     });
-    res.send({ error: error.message,
-      errorStack: error.stack });
+  } catch (error) {
+    console.log(`Error Message: ${error.message}; Error Stack: ${error.stack}`);
+    res.send({ error: "Failed Connection, check the logs." });
+    log(errorLogType, error.message);
   }
 }
 
