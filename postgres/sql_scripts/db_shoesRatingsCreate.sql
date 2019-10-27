f1f8523ed5f7a8504982c7b001a1dfd50a394a42
dernPerkins <deanperkins93@gmail.com>
2019-10-26 20:33:29 -0400

    Added addShoeRating function

--- postgres/sql_scripts/db_shoesRatingsCreate.sql
+++ postgres/sql_scripts/db_shoesRatingsCreate.sql
@@ -1,5 +1,25 @@
+-- Create Table
 create table ShoeRatings (
 ID SERIAL PRIMARY KEY,
 ShoeID int not null,
 Rating int not null
 );
+
+-- Create Functions
+CREATE FUNCTION addShoeRating(_name Shoes.Name%TYPE,
+   _rating ShoeRatings.Rating%TYPE) RETURNS BOOLEAN AS $$
+   DECLARE
+      _shoeID Shoes.ID%TYPE;
+   BEGIN
+      IF EXISTS(SELECT s.ID FROM Shoes s WHERE s.Name = _name)
+      THEN
+         -- If we find the ID from matching the Name we can insert a new rating
+         SELECT s.ID INTO _shoeID FROM Shoes s WHERE s.Name = _name;
+         INSERT INTO ShoeRatings (ShoeID, Rating) VALUES (_shoeID, _rating);
+         RETURN TRUE;
+      ELSE
+         -- Return 0 as a false in the case where the Name doesn't exist
+         RETURN FALSE;
+      END IF;
+   END;
+$$ LANGUAGE plpgsql;
